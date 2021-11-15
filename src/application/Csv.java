f358aeb0128301d8c123e47740f5a5ea349f682e
Juifaiolo <axel.schreurs@hotmail.fr>
2021-11-15 21:01:45 +0100

    projet test

---
+++ src/application/Csv.java
@@ -0,0 +1,100 @@
+package application;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Csv 
+{
+	public static List<List<String>> getListFromExcel(String fileName)
+	{
+		
+		List<List<String>> array = new ArrayList<List<String>>();
+		List<String> values = new ArrayList<String>();
+		String value = "";
+        try
+        {
+            FileInputStream file = new FileInputStream(new File(fileName));
+ 
+            //Create Workbook instance holding reference to .xlsx file
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+ 
+            //Get first/desired sheet from the workbook
+            XSSFSheet sheet = workbook.getSheetAt(0);
+ 
+            //Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()) 
+            {
+            	values.clear();
+                Row row = rowIterator.next();
+                //For each row, iterate through all the columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()) 
+                {
+                	
+                    Cell cell = cellIterator.next();
+                    //Check the cell type and format accordingly
+                    switch (cell.getCellType()) 
+                    {
+                        case NUMERIC:
+                        	 if (DateUtil.isCellDateFormatted(cell)) 
+                        	 {
+                                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                                 value = dateFormat.format(cell.getDateCellValue());
+                                 //System.out.print(dateFormat.format(cell.getDateCellValue()) + "\t");
+                             } else 
+                             {
+                                value = BigDecimal.valueOf(cell.getNumericCellValue()).toPlainString();
+                             }
+                            break;
+                        case STRING:
+                            value = cell.getStringCellValue();
+                            break;
+                        case BOOLEAN:
+                        	value = String.valueOf(cell.getBooleanCellValue());
+                            break;
+                        case FORMULA:
+                        	 value = cell.getCellFormula();
+                             break;
+                        case BLANK:
+                        	value =" ";
+                        	break;
+                        case _NONE:
+                        	value =" ";
+                        	break;
+                        case ERROR:
+                        	value =" ";
+                        	break;
+                        	
+                        default:
+                        	break;
+                    }
+
+                    
+                    values.add(value); 
+                }
+                array.add(List.copyOf(values));
+            }
+            file.close();
+            workbook.close();
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+        
+        return array;
+		
+	}
+}
---
+++ src/application/SampleController.java
@@ -0,0 +1,140 @@
+package application;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.apache.commons.compress.utils.FileNameUtils;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+
+public class SampleController implements Initializable
+{
+	private List<List<String>> array ;
+	public List<List<String>> getArray() {
+		return array;
+	}
+
+	public void setArray(List<List<String>> array) {
+		this.array = array;
+	}
+	private List<Integer> indexes = new ArrayList<Integer>();
+	@FXML
+	private Label labelNom;
+	
+	@FXML
+	private Label labelPrenom;
+	
+	@FXML
+	private Label labelDDN;
+	
+	@FXML
+	private Label labelSexe;
+	
+	@FXML
+	private Label labelMail;
+	
+	@FXML
+	private Label labelTel;
+	
+	@FXML
+	private Label labelMed;
+	
+	@FXML 
+	private Button buttonOk;
+	
+	@FXML
+	private TextField textRecherche;
+	
+	@FXML
+	private ListView<String> listPat;
+	@FXML
+	private Button testChoose;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) 
+	{
+		// TODO Auto-generated method stub
+		buttonOk.setDisable(true);
+	}
+	
+	public void recherche()
+	{
+		
+		listPat.getItems().clear();
+		indexes.clear();
+		
+		String value = textRecherche.getText();
+		
+		for (int i = 2; i < array.size(); i++) // commence à 2 car 0 = PENTACAM et  1 = nom des colonnes
+		{
+			if(array.get(i).get(0).toUpperCase().contains(value.toUpperCase()) || array.get(i).get(1).toUpperCase().contains(value.toUpperCase()) )
+			{
+				listPat.getItems().add(array.get(i).get(0) + " "+array.get(i).get(1));
+				indexes.add(i);
+			}
+		}
+	}
+	
+	public void affichePatient()
+	{
+		try 
+		{
+			if(listPat.getSelectionModel().getSelectedIndex() >=0)
+			{
+				int selected = listPat.getSelectionModel().getSelectedIndex();
+				int index = indexes.get(selected);
+				labelNom.setText(array.get(index).get(0));
+				labelPrenom.setText(array.get(index).get(1));
+				labelDDN.setText(array.get(index).get(3));
+			}
+			
+		} catch (IndexOutOfBoundsException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void chooseFile()
+	{
+		try
+		{
+			FileChooser fc = new FileChooser();
+			fc.setTitle("Veuillez sélectionner un fichier .xlxs.");
+			File file = fc.showOpenDialog(null);
+			
+			if(file != null)
+			{
+				String extension= FileNameUtils.getExtension(file.getAbsolutePath());
+				if(extension.equals("xlsx"))
+				{
+					setArray(Csv.getListFromExcel(file.getAbsolutePath()));
+					buttonOk.setDisable(false);
+					listPat.getItems().clear();
+					textRecherche.clear();
+					recherche();
+				}
+				else
+				{
+					Alert alert = new Alert(Alert.AlertType.ERROR);
+					alert.setContentText("Veuillez sélectionner un fichier xslx");
+					alert.show();
+				}	
+			}	
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	public void enterPressed()
+	{
+		
+	}
+}
